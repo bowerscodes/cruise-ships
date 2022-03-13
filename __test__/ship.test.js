06744ad3525d203b95d0493aef7d7e5ea3bdca5a
James Bowers <jamesbowers123@gmail.com>
2022-03-13 10:51:42 +0000

    add startingPort test

--- __test__/ship.test.js
+++ __test__/ship.test.js
@@ -4,4 +4,9 @@ describe('constructor', () => {
     it("constructs a new Ship object with a name and starting port", () => {
         expect(new Ship('Titanic', 'Portsmouth')).toBeInstanceOf(Object);
     });
+    it("has a starting port", () => {
+        const ship = new Ship('Titanic', 'Portsmouth');
+
+        expect(ship.startingPort).toEqual('Portsmouth');
+    });
 });
