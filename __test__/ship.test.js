97f74cf6ca5ce2abd865d9cf6ee508aac9b83213
James Bowers <jamesbowers123@gmail.com>
2022-03-16 09:16:43 +0000

    refactor

--- __test__/ship.test.js
+++ __test__/ship.test.js
@@ -2,12 +2,12 @@ const Ship = require('../src/Ship')
 
 describe('Constructor', () => {
     it('initialises a new object', () => {
-        const ship = new Ship();
-        expect(new Ship('Portsmouth')).toBeInstanceOf(Ship);
+        const ship = new Ship('Portsmouth');
+        expect(new Ship()).toBeInstanceOf(Ship);
     });
     it('has a starting port', () => {
         const ship = new Ship('Portsmouth');
         expect(ship.startingPort).toEqual('Portsmouth');
     });
-    
+
 })
