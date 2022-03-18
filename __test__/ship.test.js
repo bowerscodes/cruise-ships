4484e50ff56bdb7918121602518aecbd7d8c6bfe
James Bowers <jamesbowers123@gmail.com>
2022-03-18 11:19:32 +0000

    isolate Ship tests with Spies

--- __test__/ship.test.js
+++ __test__/ship.test.js
@@ -4,46 +4,50 @@ const Itinerary = require('../src/Itinerary')
 
 describe('Ship constructor', () => {
     describe('with ports and an itnerary', () => {
-        let ship;
         let portsmouth;
         let iow;
-        let itinerary;
 
         beforeEach(() => {
-            portsmouth = new Port('Portsmouth');
-            iow = new Port('Isle of Wight');
+            portsmouth = {
+                addShip: jest.fn(),
+                removeShip: jest.fn(),
+                name: 'Portsmouth',
+                ships: []
+            };
+            iow = {
+                addShip: jest.fn(),
+                removeShip: jest.fn(),
+                name: 'Isle of Wight',
+                ships: []
+            };
+
             itinerary = new Itinerary([portsmouth, iow]);
             ship = new Ship(itinerary);      
         });
         it('can be instantiated', () => {
-    
             expect(ship).toBeInstanceOf(Object);
         });
         it('gets added to a port upon being instantiated', () => {
-    
-            expect(portsmouth.ships).toContain(ship);
+
+            expect(portsmouth.addShip).toHaveBeenCalledWith(ship);
         });
         it('has a starting port', () => {
-            
             expect(ship.currentPort).toBe(portsmouth);
         });
         it('can set sail', () => {
-    
             ship.setSail();
     
             expect(ship.currentPort).toBeFalsy();
-            expect(portsmouth.ships).not.toContain(ship);
+            expect(portsmouth.removeShip).toHaveBeenCalledWith(ship);
         });
         it('can dock at a different port', () => {
-    
             ship.setSail();
             ship.dock();
     
             expect(ship.currentPort).toBe(iow);
-            expect(iow.ships).toContain(ship);
+            expect(iow.addShip).toHaveBeenCalledWith(ship);
         });
-        it("can't sail further than its itinerary", () => {
-    
+        it("can't sail further than its itinerary", () => {    
             ship.setSail();
             ship.dock();
     
