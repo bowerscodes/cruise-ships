62bd6adce9cdf9e8f741e00d086de7f873b53e2e
James Bowers <jamesbowers123@gmail.com>
2022-03-18 10:20:34 +0000

    refactor Port tests

--- __test__/Port.test.js
+++ __test__/Port.test.js
@@ -15,28 +15,17 @@ describe('port', () => {
             maryRose = {};
         });
         it('can be instantiated', () => {
-            const port = new Port('Belfast');
-    
             expect(new Port).toBeInstanceOf(Port);
         });
         it('has a name', () => {
-            const port = new Port('Belfast');
-    
             expect(port.name).toBe('Belfast');
         });
         it('can add ships', () => {
-            const port = new Port('Belfast');
-            const ship = {};
-    
             port.addShip(ship);
     
             expect(port.ships).toContain(ship);
         });
         it('can remove ships', () => {
-            const port = new Port('Belfast');
-            const titanic = {};
-            const maryRose = {};
-    
             port.addShip(titanic);
             port.addShip(maryRose);
             port.removeShip(maryRose);
